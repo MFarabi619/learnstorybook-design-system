236c506fd662423b7dc9d28aede5ae34729e2539
Mumtahin Farabi <mfarabi619@gmail.com>
2024-05-26 15:47:13 -0400

    Revert "feat(Button): Change border and font size"
    This reverts commit f96e4eb03cab34d3d21eb24e81cff80e00317a86.

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -33,7 +33,7 @@ const SIZES = {
 };
 
 const StyledButton = styled.button`
-  border: 10px solid red;
+  border: 0;
   border-radius: 3em;
   cursor: pointer;
   display: inline-block;
@@ -53,8 +53,7 @@ const StyledButton = styled.button`
   background: transparent;
 
   font-size: ${(props) =>
-    // props.size === SIZES.SMALL ? typography.size.s1 : typography.size.s2}px;
-    20}px
+    props.size === SIZES.SMALL ? typography.size.s1 : typography.size.s2}px;
   font-weight: ${typography.weight.extrabold};
   line-height: 1;
 
